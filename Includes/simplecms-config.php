66fe47af692c754c83420da918ed61294d12d248
darojuv <darojuv@gmail.com>
2014-04-12 14:51:09 +0000

    411

---
+++ Includes/simplecms-config.php
@@ -0,0 +1,9 @@
+<?php
+    define('DB_NAME', 'darojuvphp');
+    define('DB_USER', 'b14767af6c4cca');
+    define('DB_PASSWORD', '957775d2');
+    define('DB_HOST', 'us-cdbr-azure-west-a.cloudapp.net');
+
+    define('DEFAULT_ADMIN_USERNAME', 'darojuv');
+    define('DEFAULT_ADMIN_PASSWORD', 'Gopalachary2');
+?>
